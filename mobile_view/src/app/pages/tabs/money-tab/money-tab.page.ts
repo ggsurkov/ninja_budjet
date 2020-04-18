100f659e73f2e739a30848f57c11b2db3481ac1a
Georgy Surkov <ggsurkov@gmail.com>
2020-04-18 23:59:36 +0300

    add calculating logic

--- mobile_view/src/app/pages/tabs/money-tab/money-tab.page.ts
+++ mobile_view/src/app/pages/tabs/money-tab/money-tab.page.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Keyboard} from '@ionic-native/keyboard';
 
 @Component({
@@ -6,20 +6,59 @@ import {Keyboard} from '@ionic-native/keyboard';
     templateUrl: 'money-tab.page.html',
     styleUrls: ['money-tab.page.scss']
 })
-export class MoneyTabPage implements OnInit, OnDestroy {
-    private isMoneyBackBtnShow: boolean = false;
-    private writeOffMoney: number;
+export class MoneyTabPage {
+    private writeOffMoneyCount: number;
+    private writeOffMoneyString: string = '0';
+    private returnPaymentValue: string = '';
+    private plannedBudget: number = 15000;
+    private plannedBudgetDay: number = 500;
 
     constructor(public keyboard: Keyboard) {
     }
 
-    ngOnInit(): void {
-        this.keyboard.isVisible = true;
-        this.keyboard.show();
+    public calculate(keyNumber: string): void {
+        if (this.writeOffMoneyString === '0') {
+            this.writeOffMoneyString = '';
+        }
+        if (this.returnPaymentValue !== '') {
+            this.returnPaymentValue = '';
+        }
+        this.writeOffMoneyString += keyNumber;
     }
 
-    ngOnDestroy(): void {
-        this.keyboard.hide();
+    public delete(): void {
+        this.writeOffMoneyString = this.writeOffMoneyString.slice(0, -1);
+        if (this.writeOffMoneyString === '') {
+            this.writeOffMoneyString = '0';
+        }
     }
 
+    public enter(): void {
+        if (this.writeOffMoneyString === '0') {
+            return;
+        }
+        this.writeOffMoneyCount = parseFloat(this.writeOffMoneyString);
+        this.plannedBudget -= this.writeOffMoneyCount;
+        this.plannedBudgetDay -= this.writeOffMoneyCount;
+        this.returnPaymentValue = this.writeOffMoneyString;
+        this.writeOffMoneyString = '0';
+    }
+
+    public calculateSeparator(): void {
+        if (this.writeOffMoneyString.search(',')) {
+            return;
+        }
+        this.writeOffMoneyString += ',';
+    }
+
+    public returnPayment(): void {
+        this.writeOffMoneyCount = parseFloat(this.returnPaymentValue);
+        this.plannedBudget += this.writeOffMoneyCount;
+        this.plannedBudgetDay += this.writeOffMoneyCount;
+        this.returnPaymentValue = '';
+    }
+
+    public cleanAll() {
+        this.writeOffMoneyString = '0';
+    }
 }
